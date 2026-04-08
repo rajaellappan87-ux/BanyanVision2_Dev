560a16bec8c54b1edbf2177eed2b777a9d3f3214
RajaE-MindFox <rajaellappan87@gmail.com>
2026-04-09 00:02:08 +0530

    changed username

--- deploy-to-prod.bat
+++ deploy-to-prod.bat
@@ -8,7 +8,7 @@ echo.
 
 cd /d D:\Raja\BV\SourceCode\BanyanVision2_Dev
 
-git config user.name "rajaellappan87-ux"
+git config user.name "RajaE-MindFox"
 git config user.email "rajaellappan87@gmail.com"
 
 echo [1/7] Current branch status...
